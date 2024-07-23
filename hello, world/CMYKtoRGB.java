6aede85aeb0fa273d54d2d94b168fe10c3e20f93
Sihle <sshabangu061@gmail.com>
2024-07-23 23:26:11 +0200

    Right Triangle fixed

--- hello, world/CMYKtoRGB.java
+++ hello, world/CMYKtoRGB.java
@@ -1,17 +1,14 @@
-import static java.lang.Double.parseDouble;
-import static java.lang.Math.*;
-
 public class CMYKtoRGB {
     public static void main(String[] args) {
-        double c = parseDouble(args[0]);
-        double m = parseDouble(args[1]);
-        double y = parseDouble(args[2]);
-        double k = parseDouble(args[3]);
+        double c = Double.parseDouble(args[0]);
+        double m = Double.parseDouble(args[1]);
+        double y = Double.parseDouble(args[2]);
+        double k = Double.parseDouble(args[3]);
         double white = 1 - k;
 
-        int red = (int) round(255 * white * (1 - c));
-        int green = (int) round(255 * white * (1 - m));
-        int blue = (int) round(255 * white * (1 - y));
+        int red = (int) Math.round(255 * white * (1 - c));
+        int green = (int) Math.round(255 * white * (1 - m));
+        int blue = (int) Math.round(255 * white * (1 - y));
 
         System.out.println("red   = " + red);
         System.out.println("green = " + green);
--- hello, world/GreatCircle.java
+++ hello, world/GreatCircle.java
@@ -1,27 +1,24 @@
-import static java.lang.Double.parseDouble;
-import static java.lang.Math.*;
-
 public class GreatCircle {
     public static void main(String[] args) {
         // get the degrees
-        double a = parseDouble(args[0]);
-        double b = parseDouble(args[1]);
-        double c = parseDouble(args[2]);
-        double d = parseDouble(args[3]);
+        double a = Double.parseDouble(args[0]);
+        double b = Double.parseDouble(args[1]);
+        double c = Double.parseDouble(args[2]);
+        double d = Double.parseDouble(args[3]);
         double r = 6371.0;
 
         // convert degrees to radians
-        double x1 = toRadians(a);
-        double x2 = toRadians(b);
-        double y1 = toRadians(c);
-        double y2 = toRadians(d);
+        double x1 = Math.toRadians(a);
+        double x2 = Math.toRadians(b);
+        double y1 = Math.toRadians(c);
+        double y2 = Math.toRadians(d);
 
         // calculate the brackets
         double xBracket = (x2 - x1) / 2;
         double yBracket = (y2 - y1) / 2;
 
         // calculate the math things
-        double distance = (2 * r) * asin(sqrt(pow(sin(xBracket), 2) + cos(x1) * cos(x2) * pow(sin(yBracket), 2)));
+        double distance = (2 * r) * Math.asin(Math.sqrt(Math.pow(Math.sin(xBracket), 2) + Math.cos(x1) * Math.cos(x2) * Math.pow(Math.sin(yBracket), 2)));
 
         System.out.println(distance + " kilometers");
     }
