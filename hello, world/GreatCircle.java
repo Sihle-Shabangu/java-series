577c4c925d2b9ae01f45b8bb3edb09dc4e5e1dc3
Sihle <sshabangu061@gmail.com>
2024-07-24 10:02:44 +0200

    Finally Donegit add GreatCircle.java

--- hello, world/GreatCircle.java
+++ hello, world/GreatCircle.java
@@ -9,16 +9,15 @@ public class GreatCircle {
 
         // convert degrees to radians
         double x1 = Math.toRadians(a);
-        double x2 = Math.toRadians(b);
-        double y1 = Math.toRadians(c);
+        double y1 = Math.toRadians(b);
+        double x2 = Math.toRadians(c);
         double y2 = Math.toRadians(d);
 
-        // calculate the brackets
-        double xBracket = (x2 - x1) / 2;
-        double yBracket = (y2 - y1) / 2;
+        double xBracket = x2 - x1;
+        double yBracket = y2 - y1;
 
         // calculate the math things
-        double distance = (2 * r) * Math.asin(Math.sqrt(Math.pow(Math.sin(xBracket), 2) + Math.cos(x1) * Math.cos(x2) * Math.pow(Math.sin(yBracket), 2)));
+        double distance = r * 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(xBracket / 2),2) + Math.cos(x1) * Math.cos(x2) * Math.pow(Math.sin(yBracket / 2),2)));
 
         System.out.println(distance + " kilometers");
     }
