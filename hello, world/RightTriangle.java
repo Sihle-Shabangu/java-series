56d22ff81b7e41db918d5125e37413658b27c47a
Sihle <sshabangu061@gmail.com>
2024-07-24 10:36:00 +0200

    Right triangle fixed

--- hello, world/RightTriangle.java
+++ hello, world/RightTriangle.java
@@ -1,12 +1,16 @@
 public class RightTriangle {
     public static void main(String[] args) {
-            int a = Integer.parseInt(args[0]);
-            int b = Integer.parseInt(args[1]);
-            int c = Integer.parseInt(args[2]);
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
 
-            boolean positive = a > 0 && b > 0 && c > 0;
-            boolean expression = a * a + b * b == c * c;
+        int min    = Math.min(a, Math.min(b, c));
+        int max    = Math.max(a, Math.max(b, c));
+        int median = a + b + c - min - max;
 
-            System.out.println(positive == expression && !positive);
+        boolean positive = a > 0 && b > 0 && c > 0;
+        boolean expression = min * min + median * median == max * max;
+
+        System.out.println(positive == expression && positive);
     }
 }
